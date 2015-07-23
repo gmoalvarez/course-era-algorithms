a910b9e667609fdc25ec61abe9640481739656f4
Guillermo Alvarez <galvarezcity@me.com>
2015-07-23 10:21:23 -0700

    completed draw method and created small unit test that works to plot points

--- src/PointSET.java
+++ src/PointSET.java
@@ -36,7 +36,10 @@ public class PointSET {
     }            // does the set contain point p?
 
     public void draw() {
-
+        StdDraw.setPenColor(StdDraw.BLACK);
+        StdDraw.setPenRadius(.01);
+        for (Point2D p : points)
+            StdDraw.point(p.x(),p.y());
     }                         // draw all points to standard draw
 
     public Iterable<Point2D> range(RectHV rect) {
@@ -48,6 +51,26 @@ public class PointSET {
     }             // a nearest neighbor in the set to point p; null if the set is empty
 
     public static void main(String[] args) {
+        int N = 20;
 
-    }                  // unit testing of the methods (optional)
+        Point2D[] arrayOfPoints = new Point2D[N];
+        for (int i = 0; i < N; i++) {
+            arrayOfPoints[i] = new Point2D(StdRandom.uniform(0.0,1.0),StdRandom.uniform(0.0,1.0));
+        }
+
+        PointSET drawPoints = new PointSET();
+        for (int i = 0; i < N; i++) {
+            System.out.println(arrayOfPoints[i]);
+            drawPoints.insert(arrayOfPoints[i]);
+        }
+        System.out.println("The size is(should be 20): " + drawPoints.size());
+        System.out.println("Is the set empty? (should be false):" + drawPoints.isEmpty());
+        System.out.println("Does the set contain (0.43212,0.121)? (probably false):"
+                + drawPoints.contains(new Point2D(0.43212, 0.121)));
+        System.out.println("Does the set contain arrayOfPoints[3] (should be true):"
+                + drawPoints.contains(arrayOfPoints[3]));
+        System.out.println("Draw points");
+        drawPoints.draw();
+    } // unit testing of the methods (optional)
 }
+
