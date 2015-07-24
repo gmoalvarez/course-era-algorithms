f4a07f3fbe797480e073125c2f70a70dcf9c14da
Guillermo Alvarez <galvarezcity@me.com>
2015-07-23 17:01:39 -0700

    Completed first stage of insert() method. I still need to test it so I will write the contains method next and test. I also still need to implement the rectHV part of insert as well.

--- src/KdTree.java
+++ src/KdTree.java
@@ -1,9 +1,14 @@
+import java.util.Comparator;
 
 public class KdTree {
 
     private int size;
     private Node root;
+    private boolean compareX;
 
+    private enum Orientation {
+        compareX, compareY,
+    }
 
     private static class Node {
         private Point2D p;      // the point
@@ -13,9 +18,11 @@ public class KdTree {
     }
 
     public KdTree() {
-    }
+        size = 0;
+        root = null;
+        compareX = true;
+    } // construct an empty set of points
 
-    // construct an empty set of points
     public boolean isEmpty() {
         return size() == 0;
     }                      // is the set empty?
@@ -26,7 +33,34 @@ public class KdTree {
 
     public void insert(Point2D p) {
 
+        root = insert(root, p, Orientation.compareX);
     }              // add the point to the set (if it is not already in the set)
+
+    private Node insert(Node node, Point2D point, Orientation orientation) {
+        if (node == null) {
+            Node tempNode = new Node();
+            tempNode.p = point;
+//            tempNode.rect=???
+            return tempNode;
+        }
+        if (node.p.equals(point)) {
+            StdOut.println("Attempting to insert equal point, return");
+            return null;
+        }
+        switch (orientation) {
+            case compareX:
+                Comparator<Point2D> cmpX = Point2D.X_ORDER;
+                int compX = cmpX.compare(point, node.p);
+                if (compX < 0) node.lb = insert(node.lb, point, Orientation.compareY);
+                else if (compX >= 0) node.rt = insert(node.rt, point, Orientation.compareY);
+            case compareY:
+                Comparator<Point2D> cmpY = Point2D.Y_ORDER;
+                int compY = cmpY.compare(point, node.p);
+                if (compY < 0) node.lb = insert(node.lb, point, Orientation.compareY);
+                else if (compY >= 0) node.rt = insert(node.rt, point, Orientation.compareY);
+        }
+        return node;
+    }
 
     public boolean contains(Point2D p) {
         return false;
